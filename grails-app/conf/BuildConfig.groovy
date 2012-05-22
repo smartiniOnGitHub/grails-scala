5fee57a45adc1b523c1c4d64238a40b5bc563b9a
Domingo Suarez Torres <domingo.suarez@gmail.com>
2012-05-22 10:55:58 -0500

    cleanup files according Grails 2.0.3 release

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,7 +1,9 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
+grails.project.target.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
+
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
@@ -27,4 +29,18 @@ grails.project.dependency.resolution = {
                 "org.scala-lang:scala-library:$scalaVersion"
         compile('org.codehaus.groovy.modules:groovytransforms:0.2') { transitive = false }
     }
+    plugins {
+        /*
+        This validation is for prevent load the following plugins in previous Grails versions.
+        I some Grails versions from 1.3.* the 'export = false' does not work. For Grails 2.* works properly
+        */
+        if (grailsVersion.startsWith('2')) {
+          build(":release:2.0.0") { export = false }
+          build(":rest-client-builder:1.0.2") { export = false }
+        }
+      }
 }
+
+
+grails.release.scm.enabled = false
+grails.project.repos.default = "grailsCentral"
