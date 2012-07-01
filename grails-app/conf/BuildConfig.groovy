22ba938892bc740566ef6e7e13a84e91294ec959
Domingo Suarez Torres <domingo.suarez@gmail.com>
2012-06-30 23:34:33 -0500

    formatted out the config file

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -5,41 +5,41 @@ grails.project.target.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
-    inherits("global") {
-        // uncomment to disable ehcache
-        // excludes 'ehcache'
-    }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    repositories {
-        grailsPlugins()
-        grailsHome()
-        grailsCentral()
+  // inherit Grails' default dependencies
+  inherits("global") {
+    // uncomment to disable ehcache
+    // excludes 'ehcache'
+  }
+  log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+  repositories {
+    grailsPlugins()
+    grailsHome()
+    grailsCentral()
 
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        mavenLocal()
-        mavenCentral()
+    // uncomment the below to enable remote dependency resolution
+    // from public Maven repositories
+    mavenLocal()
+    mavenCentral()
+  }
+  dependencies {
+    def scalaVersion = '2.9.1'
+    build "org.scala-lang:scala-compiler:$scalaVersion",
+      "org.scala-lang:scala-library:$scalaVersion"
+    compile "org.scala-lang:scala-compiler:$scalaVersion",
+      "org.scala-lang:scala-library:$scalaVersion"
+    compile('org.codehaus.groovy.modules:groovytransforms:0.2') { transitive = false }
+  }
+  plugins {
+    /*
+    This validation is for prevent load the following plugins in previous Grails versions.
+    I some Grails versions from 1.3.* the 'export = false' does not work. For Grails 2.* works properly
+    */
+    if (grailsVersion.startsWith('2')) {
+      build(":release:2.0.3") { export = false }
+      build(":rest-client-builder:1.0.2") { export = false }
+      build ":tomcat:$grailsVersion"
     }
-    dependencies {
-		def scalaVersion = '2.9.1'
-        build "org.scala-lang:scala-compiler:$scalaVersion",
-              "org.scala-lang:scala-library:$scalaVersion"
-        compile "org.scala-lang:scala-compiler:$scalaVersion",
-                "org.scala-lang:scala-library:$scalaVersion"
-        compile('org.codehaus.groovy.modules:groovytransforms:0.2') { transitive = false }
-    }
-    plugins {
-        /*
-        This validation is for prevent load the following plugins in previous Grails versions.
-        I some Grails versions from 1.3.* the 'export = false' does not work. For Grails 2.* works properly
-        */
-        if (grailsVersion.startsWith('2')) {
-          build(":release:2.0.3") { export = false }
-          build(":rest-client-builder:1.0.2") { export = false }
-          build ":tomcat:$grailsVersion"
-        }
-      }
+  }
 }
 
 
